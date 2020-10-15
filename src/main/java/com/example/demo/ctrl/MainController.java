42cb7c39ea9858ae0ab4224309bcf2c8319be5ed
l2jong <gkwkdlawoh@naver.com>
2020-10-15 19:36:36 +0900

    init()

---
+++ src/main/java/com/example/demo/ctrl/MainController.java
@@ -0,0 +1,15 @@
+package com.example.demo.ctrl;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/")
+	public ModelAndView main() {
+		ModelAndView mav = new ModelAndView("main");
+		return mav;
+	}
+}
